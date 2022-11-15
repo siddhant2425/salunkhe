97bf6c8388a0f1dcfe0bb01b6434bdd0bc44f7c4
siddhant2425 <118192889+siddhant2425@users.noreply.github.com>
2022-11-15 18:37:36 +0530

    Add files via upload

---
+++ xy.py
@@ -0,0 +1,14 @@
+
+
+print("hi")
+
+
+class student:
+    def __init__(self,name,roll):
+        self.m=name
+        self.r=roll
+    def show(self):
+        print(self.n)
+        print(self.r)
+        s1=student('sid',25)
+        s1.show()
